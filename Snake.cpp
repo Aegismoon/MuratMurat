a761727447006f2cf8294dd29a0363c850e4341a
aegismoon <aegismoon@yandex.ru>
2015-10-18 12:17:27 +0500

    First step before snake game random symbol '*' coordinates established

---
+++ Snake.cpp
@@ -0,0 +1,57 @@
+////////////////////////////////////////////
+//	Concole application C++		          //
+//					                      //
+//	    <OOP-geekbrains-Snake>            //
+//					                      //
+//	 Author Dmitry Murat		          //
+///////////////////////////////////////////
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+typedef unsigned int UINT;
+
+void gotoXY( int column, int line )
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
+    );
+}
+
+int randomX()
+{
+    srand(time(NULL));
+
+    return rand()%10+1;
+
+}
+
+int randomY(int (*ptr)())
+{
+    return ptr();
+}
+
+int main( int argc, char* argv[])
+{
+    int x(0), y(0);
+    char symb = '*';
+
+    while(1)
+    {
+        x = randomX();
+        y = randomY(randomX);
+        gotoXY(x,y);
+        cout << symb;
+        system("cls");
+    }
+
+    return 0;
+}
